d67b547f1c1d90392e4c56854e1bd4406f01299a
alanwalk <alanwalk@qq.com>
2020-11-06 15:07:22 +0800

    released 0.1.3

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # tsc-with-cleanup
-A tool for TypeScript.
+A simple script that that can be used to cleanup previously transpiled typescript files.
+
+## Install
+```
+npm install tsc-with-cleanup --save-dev
+```
+
+## Usage
+```
+tsc-with-cleanup -s src -d dist -w -rd -v
+```
+
+## Options
+```
+-s, --src <srcDir>				sets the source folder with ts files (default: src)
+-d, --dist <distDir>			sets the distribution folder with js files (default: dist)
+-e, --exclude [relativePath...]	files to exclude on remove
+-w, --watch						whether to watch for files being deleted (default: false)
+-v, --verbose					Whether to show messages (default: false)
+-if, --ifDeclared				If set to true, js files in dist will only be removed if a file with the same name and a .d.ts extension is also present (default: false)
+-rd, --removeDirs				whether to remove empty directories (default: false)
+...
+Any tsc options
+```
+
+## Reference
+- [ts-cleanup](https://www.npmjs.com/package/ts-cleanup)
+- [ts-cleaner](https://www.npmjs.com/package/ts-cleaner)
+- [tsc-watch](https://www.npmjs.com/package/tsc-watch)
--- src/bin.ts
+++ src/bin.ts
@@ -8,25 +8,31 @@ const pkg = require("../package.json");
 commander
     .name(pkg.name)
     .description(pkg.description)
+    .usage("tsc-with-cleanup -s src -d dist -w -rd -v")
     .version(pkg.version, "-V, --version", "output the current version")
-    .option("-s, --src <srcDir>", "sets the source folder with ts files (auto load from tsconfig.json)", "src")
-    .option("-d, --dist <distDir>", "sets the distribution folder with js files (auto load from tsconfig.json)", "dist")
+    .option("-s, --src <srcDir>", "sets the source folder with ts files (default: src)", "src")
+    .option("-d, --dist <distDir>", "sets the distribution folder with js files (default: dist)", "dist")
     .option("-e, --exclude [relativePath...]", "files to exclude on remove")
-    .option("-w, --watch", "whether to watch for files being deleted (disabled by default)", false)
-    .option("-v, --verbose", "Whether to show messages (disabled by default)", false)
-    .option("-if, --ifDeclared", "If set to true, js files in dist will only be removed if a file with the same name and a .d.ts extension is also present (disabled by default)", false)
-    .option("-rd, --removeDirs", "whether to remove empty directories (disabled by default)", false)
-    // .allowUnknownOption()
+    .option("-w, --watch", "whether to watch for files being deleted (default: false)", false)
+    .option("-v, --verbose", "Whether to show messages (default: false)", false)
+    .option("-if, --ifDeclared", "If set to true, js files in dist will only be removed if a file with the same name and a .d.ts extension is also present (default: false)", false)
+    .option("-rd, --removeDirs", "whether to remove empty directories (default: false)", false)
+    .allowUnknownOption()
     .parse(process.argv);
 
-function getTscOpt() : string[] {
+function getTscOpt(): string[] {
     let tscOpts = process.argv.splice(2) // trim node tsc-with-cleanup
-    let optFmts : commander.Option[] = commander.options
+    let opts = commander.opts();
+    let optFmts: commander.Option[] = commander.options
     optFmts.forEach(optFmt => {
-        if (optFmt.short !== "-w") {
-            let index = tscOpts.indexOf(optFmt.short as string)
-            if (index <= 0) index = tscOpts.indexOf(optFmt.long)
-            if (index > -1) tscOpts.splice(index, optFmt.required ? 2 : 1)
+        if (optFmt.short === "-w") return;
+        let index = tscOpts.indexOf(optFmt.short as string)
+        if (index < 0) index = tscOpts.indexOf(optFmt.long)
+        if (index < 0) return;
+        if (optFmt.short === "-e") {
+            tscOpts.splice(index, opts.exclude.length + 1);
+        } else {
+            tscOpts.splice(index, optFmt.required ? 2 : 1);
         }
     });
     return tscOpts
